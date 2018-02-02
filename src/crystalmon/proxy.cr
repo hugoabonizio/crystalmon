9ae92cca14997f9fb10f9a3227699bdad36c5bbc
Hugo Abonizio <hugo_abonizio@hotmail.com>
2018-02-02 14:18:45 -0200

    feat: Change to TCPServer instead of HTTP

--- src/crystalmon/proxy.cr
+++ src/crystalmon/proxy.cr
@@ -1,45 +1,25 @@
 require "socket"
-require "http"
 
 module Crystalmon
   class Proxy
     def initialize(port : Int32, target_port : Int32)
-      @server = HTTP::Server.new(port) do |ctx|
-        handle(ctx)
-      end
+      @server = TCPServer.new(port)
       @target_port = target_port
     end
 
     def listen
-      @server.listen
+      while client = @server.accept?
+        spawn handle(client)
+      end
     end
 
     private def handle(ctx)
-      request = ctx.request
-      target_connection = nil
-      until target_connection
-        begin
-          target_connection = HTTP::Client.new("127.0.0.1", Config.target_port)
-        rescue e
-          sleep 100.milliseconds
-        end
-      end
+      puts "request..."
+      socket = TCPSocket.new("127.0.0.1", Config.target_port)
+      spawn { IO.copy(ctx, socket) }
 
-      if target = target_connection
-        finished = false
-        until finished
-          begin
-            target.exec(request) do |response|
-              ctx.response.headers.merge!(response.headers)
-              response.consume_body_io
-              ctx.response << response.body
-              finished = true
-            end
-          rescue e
-            sleep 100.milliseconds
-          end
-        end
-      end
+      # TODO this seems to hang forever
+      spawn { IO.copy(socket, ctx) }
     end
   end
 end
